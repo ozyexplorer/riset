bd1ec3753e5afa10341c361b40ed130b12d23b41
fata97 <fatahasan97@gmail.com>
2018-03-06 14:55:39 +0700

    a

---
+++ BackEnd/latihan_1.php
@@ -0,0 +1,17 @@
+<?php
+//variabel
+//tipe data string
+$x = "Riset";
+//tipe data integer
+$angka = 1000;
+$angka2 = 100;
+
+/*Mencetak Nilai*/
+echo "<h1>$x</h1>";
+print "Latihan 1<br>";
+echo "Nilai Asli Anda = ". $angka;
+echo "<br>";
+$hasil = $angka + $angka2;
+echo "Nilai setelah bonus = ". $hasil;
+
+ ?>
